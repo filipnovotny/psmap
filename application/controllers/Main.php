90f408244d30d5a849126ffd62902ac7a508c9a9
Novotny <filipnovo@gmail.com>
2015-08-03 00:44:20 +0200

    added base url property to make twigs more portable

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,21 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 
+
 	public function index()
 	{
 		$this->load->library('Twig');
-		$this->twig->render('map', array("STATIC_URL" => STATIC_URL));
+		$this->load->helper('url');
+		$this->twig->render('map', array("STATIC_URL" => STATIC_URL, "BASE_URL" => base_url()));
 	}
 
 	public function about()
 	{
 		$this->load->library('Twig');
-		$this->twig->render('about', array("STATIC_URL" => STATIC_URL));
+		$this->twig->render('about', array("STATIC_URL" => STATIC_URL, "BASE_URL" => base_url()));
 	}
 
 	public function contact()
 	{
 		$this->load->library('Twig');
-		$this->twig->render('contact', array("STATIC_URL" => STATIC_URL));
+		$this->twig->render('contact', array("STATIC_URL" => STATIC_URL, "BASE_URL" => base_url()));
 	}
 }
