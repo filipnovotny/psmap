d79efe7329d6a824ec468c5ae319f2fada398aa7
Novotny <filipnovo@gmail.com>
2015-08-03 00:00:22 +0200

    removed machine-specific settings.php file

--- settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("FCPATH", dirname(__FILE__)."/vendor/ellislab/codeigniter/");
-define("BASEPATH", dirname(__FILE__)."/vendor/ellislab/codeigniter/system/");
-define("APPPATH", dirname(__FILE__)."/application/");
-define("VIEWPATH", APPPATH."views/");
-define('ENVIRONMENT', 'development');
-
-define('STATIC_URL', '/static/');
-?>
