c86697dc43e76c0c98572c0b118bab1c3bd8c0f0
Novotny <filipnovo@gmail.com>
2015-08-03 00:32:41 +0200

    updated gitignore and map.js to point to server for tiles

--- static/map.js
+++ static/map.js
@@ -19,7 +19,7 @@ app.controller("defaultcontroller", [ '$scope','$http', function($scope,$http) {
 			
 		});
 
-	 $http.get('http://dtserv.labscinet.com/ps/?format=json').
+	 $http.get('http://dtserv.labscinet.com/~dt/psmap/ps/?format=json').
         success(function(data) {
             var markers = [];
             for (var i in data) {
